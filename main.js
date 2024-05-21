b49e793eeb8fa03b4f4cd13ece244dc7540fd279
egorvas1122 <vasenkoegor80@gmail.com>
2024-05-21 16:39:02 -0400

    proekt5

---
+++ main.js
@@ -0,0 +1,55 @@
+let menu = document.querySelector('.fixed-menu');
+menu.addEventListener('click', function(event){
+    let clickTarget = event.target;
+    let activeBth = document.querySelector('.active');
+    if(clickTarget.classList.contains('nav-link')){
+        clickTarget.classList.add('active');
+        activeBth.classList.remove('active');
+    }
+    let body = document.querySelector('body');
+    let mobileMenu = document.querySelector('.mobile-menu');
+    if (!body.classList.contains('off-scroll') && !mobileMenu.classList.contains('hide')){
+        body.classList.add('off-scroll');
+    } 
+    else{
+        body.classList.remove('off-scroll');
+    }
+});
+let classLink = ".main_link"
+window.onscroll = function(){
+    let h = document.documentElement.clientHeight;
+    if (window.scrollY >= h*4){
+         classLink = '.comments-link';
+    }
+   else if (window.scrollY >= h*3){
+    classLink = '.works-link';
+   }
+   else if (window.scrollY >= h*2){
+    classLink = '.skills-link';
+   }
+   else if (window.scrollY >= h*1){
+    classLink = '.about-link';
+   }
+   else {classLink = '.main-link';}
+   let activeBth = document.querySelector('.active');
+    let newActiveBth = document.querySelector(classLink);
+   if (!newActiveBth.classList.contains('active')){
+        newActiveBth.classList.add('active');
+        activeBth.classList.remove('active');
+        let mobile = document.querySelector('.mobile-menu');
+        if(!mobile.classList.contains('.hide')){
+            mobile.classList.add('hide');
+            
+        }
+    }
+    
+}
+
+document.querySelector('.mobile-buttom').addEventListener('click', function(event)
+{
+    document.querySelector('.mobile-menu').classList.toggle('hide');
+    document.querySelectorAll('.mobile-btn').forEach(function(item){
+        item.classList.toggle('btn-hide');
+
+    });
+});
